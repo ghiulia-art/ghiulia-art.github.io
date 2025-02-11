25528090d6b160f0ea4abcad705d907dbdf06656
ghiulia-art <ghiulia.freitas@estudante.ifms.edu.br>
2025-02-11 16:21:25 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1 class='text-center'>ola muerndo!</h1>
+
+<svelte:head>
+	<style>
+		body {
+			background-image: url('/flor.png');
+			background-repeat: no-repeat;
+			background-size: 100%;
+		}
+	</style>
+</svelte:head>
